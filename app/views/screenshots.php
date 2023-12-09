a36d91584deb917c8f70e9424bf51d562d200ce1
Daniel Brendel <dbrendel1988@gmail.com>
2023-12-09 10:41:25 +0100

    Screenshots page

--- app/views/screenshots.php
+++ app/views/screenshots.php
@@ -2,6 +2,108 @@
     <h2>Screenshots</h2>
 
     <p class="is-font-medium">
-        Get am impression of the software.
+        Get an impression of the software.
     </p>
+
+    <div><br/><br/></div>
+
+    <h3>Dashboard</h3>
+
+    <p class="is-font-mild">
+        The dashboard provides you with locations, stats and other important information
+    </p>
+
+    <div class="page-screenshots is-clickable">
+        <div class="page-screenshot-item screenshot-mobile">
+            <img src="{{ asset('img/screenshots/Screenshot_20231023_123009_HortusFox.jpg') }}" alt="screenshot" onclick="window.open(this.getAttribute('src'));"/>
+        </div>
+
+        <div class="page-screenshot-item screenshot-mobile">
+            <img src="{{ asset('img/screenshots/Screenshot_20231023_123017_HortusFox.jpg') }}" alt="screenshot" onclick="window.open(this.getAttribute('src'));"/>
+        </div>
+
+        <div class="page-screenshot-item screenshot-mobile">
+            <img src="{{ asset('img/screenshots/Screenshot_20231023_123027_HortusFox.jpg') }}" alt="screenshot" onclick="window.open(this.getAttribute('src'));"/>
+        </div>
+    </div>
+
+    <hr/>
+
+    <h3>Plant view</h3>
+
+    <p class="is-font-mild">
+        List all plants in a location and manage plant details
+    </p>
+
+    <div class="page-screenshots is-clickable">
+        <div class="page-screenshot-item screenshot-mobile">
+            <img src="{{ asset('img/screenshots/Screenshot_20231023_123202_HortusFox.jpg') }}" alt="screenshot" onclick="window.open(this.getAttribute('src'));"/>
+        </div>
+
+        <div class="page-screenshot-item screenshot-mobile">
+            <img src="{{ asset('img/screenshots/Screenshot_20231023_123229_HortusFox.jpg') }}" alt="screenshot" onclick="window.open(this.getAttribute('src'));"/>
+        </div>
+
+        <div class="page-screenshot-item screenshot-mobile">
+            <img src="{{ asset('img/screenshots/Screenshot_20231023_123439_HortusFox.jpg') }}" alt="screenshot" onclick="window.open(this.getAttribute('src'));"/>
+        </div>
+    </div>
+
+    <hr/>
+
+    <h3>Tasks, Search & Inventory</h3>
+
+    <p class="is-font-mild">
+        Manage required tasks, search the database and maintain your inventory
+    </p>
+
+    <div class="page-screenshots is-clickable">
+        <div class="page-screenshot-item screenshot-mobile">
+            <img src="{{ asset('img/screenshots/Screenshot_20231023_123512_HortusFox.jpg') }}" alt="screenshot" onclick="window.open(this.getAttribute('src'));"/>
+        </div>
+
+        <div class="page-screenshot-item screenshot-mobile">
+            <img src="{{ asset('img/screenshots/Screenshot_20231023_123530_HortusFox.jpg') }}" alt="screenshot" onclick="window.open(this.getAttribute('src'));"/>
+        </div>
+
+        <div class="page-screenshot-item screenshot-mobile">
+            <img src="{{ asset('img/screenshots/Screenshot_20231023_123548_HortusFox.jpg') }}" alt="screenshot" onclick="window.open(this.getAttribute('src'));"/>
+        </div>
+    </div>
+
+    <hr/>
+
+    <h3>Many more features</h3>
+
+    <p class="is-font-mild">
+        Manage profile settings, exchange collaborative messages and much more!
+    </p>
+
+    <div class="page-screenshots is-clickable">
+        <div class="page-screenshot-item screenshot-mobile">
+            <img src="{{ asset('img/screenshots/Screenshot_20231023_123600_HortusFox.jpg') }}" alt="screenshot" onclick="window.open(this.getAttribute('src'));"/>
+        </div>
+
+        <div class="page-screenshot-item screenshot-mobile">
+            <img src="{{ asset('img/screenshots/Screenshot_20231023_124151_HortusFox.jpg') }}" alt="screenshot" onclick="window.open(this.getAttribute('src'));"/>
+        </div>
+
+        <div class="page-screenshot-item screenshot-mobile">
+            <img src="{{ asset('img/screenshots/Screenshot_20231023_124304_HortusFox.jpg') }}" alt="screenshot" onclick="window.open(this.getAttribute('src'));"/>
+        </div>
+    </div>
+
+    <hr/>
+
+    <h3>Comfortable administration</h3>
+
+    <p class="is-font-mild">
+        Admins can also manage the app and users
+    </p>
+
+    <div class="page-screenshots is-clickable">
+        <div class="page-screenshot-item screenshot-desktop">
+            <img src="{{ asset('img/screenshots/Screenshot 2023-12-03 221626.png') }}" alt="screenshot" onclick="window.open(this.getAttribute('src'));"/>
+        </div>
+    </div>
 </div>
