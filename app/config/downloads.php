5351eae29a4538d4155c9a5bf13ebadd42736872
Daniel Brendel <dbrendel1988@gmail.com>
2023-12-20 22:10:45 +0100

    Current version

--- app/config/downloads.php
+++ app/config/downloads.php
@@ -3,7 +3,7 @@
 return [
     [
         'name' => 'HortusFox Source',
-        'version' => 'latest: 1.2',
-        'file' => 'https://github.com/danielbrendel/hortusfox-web/releases'
+        'version' => 'latest: 1.3',
+        'file' => 'https://github.com/danielbrendel/hortusfox-web/releases/tag/v1.3'
     ]
 ];
