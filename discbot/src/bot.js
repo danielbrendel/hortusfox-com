f40c90fee536c88b0386df2e2f5a83d40e923bdd
Daniel Brendel <dbrendel1988@gmail.com>
2024-09-30 02:35:25 +0200

    Stats command & refactoring

--- discbot/src/bot.js
+++ discbot/src/bot.js
@@ -3,24 +3,8 @@ import 'dotenv/config';
 import axios from 'axios';
 import { createCache } from 'cache-manager';
 
-function cmd_plant(interaction)
-{
-    interaction.reply(`:potted_plant:`);
-}
-
 var SOFTWARE_VERSION = 'Unknown';
-function cmd_version(interaction)
-{
-    cache.wrap('software_version', async () => {
-        axios.get(process.env.WEB_BACKEND + '/software/version').then(function(response) {
-            if (response.data.code == 200) {
-                SOFTWARE_VERSION = response.data.version;
-            }
-        });
-    }, process.env.CACHE_TIME, process.env.CACHE_TIME);
-
-    interaction.reply(`Current software version: ${SOFTWARE_VERSION}`);
-}
+var GITHUB_STATS = 'Unknown';
 
 function cmd_url(interaction)
 {
@@ -37,17 +21,36 @@ function cmd_demo(interaction)
     interaction.reply(`${process.env.WEB_BACKEND}/demo`);
 }
 
+function cmd_version(interaction)
+{
+    cache.wrap('software_version', async () => {
+        axios.get(process.env.WEB_BACKEND + '/software/version').then(function(response) {
+            if (response.data.code == 200) {
+                SOFTWARE_VERSION = response.data.version;
+            }
+        });
+    }, process.env.CACHE_TIME, process.env.CACHE_TIME);
+
+    interaction.reply(`Current software version: ${SOFTWARE_VERSION}`);
+}
+
+function cmd_stats(interaction)
+{
+    cache.wrap('github_stats', async () => {
+        axios.get(process.env.WEB_REPO_API).then(function(response) {
+            GITHUB_STATS = `[GitHub Stats] Stars: ${response.data.stargazers_count} | Forks: ${response.data.forks_count} | Open Issues: ${response.data.open_issues}`;
+        });
+    }, process.env.CACHE_TIME, process.env.CACHE_TIME);
+
+    interaction.reply(GITHUB_STATS);
+}
+
+function cmd_plant(interaction)
+{
+    interaction.reply(`:potted_plant:`);
+}
+
 const commands = [
-    {
-        name: 'plant',
-        description: 'Shows a lovely plant emoji',
-        handler: cmd_plant
-    },
-    {
-        name: 'version',
-        description: 'Show current release version',
-        handler: cmd_version
-    },
     {
         name: 'url',
         description: 'Show project URL',
@@ -63,6 +66,21 @@ const commands = [
         description: 'Show link to the demo workspace',
         handler: cmd_demo
     },
+    {
+        name: 'version',
+        description: 'Show current release version',
+        handler: cmd_version
+    },
+    {
+        name: 'stats',
+        description: 'Show current GitHub repository statistics',
+        handler: cmd_stats
+    },
+    {
+        name: 'plant',
+        description: 'Shows a lovely plant emoji',
+        handler: cmd_plant
+    }
 ];
 
 function handleCommand(interaction)
