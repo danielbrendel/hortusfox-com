56fbe552d9bdaf03c4fa9923e13f3c54fe098e68
Daniel Brendel <dbrendel1988@gmail.com>
2025-03-20 22:43:05 +0100

    Improve header look

--- app/views/header.php
+++ app/views/header.php
@@ -5,20 +5,20 @@
 
             <h2>Collaborative self-hosted plant tracking & management</h2>
 
-            <div class="page-header-badges">
-                <img src="https://img.shields.io/github/stars/{{ env('LINK_REPOSITORY') }}?style=flat&color=green" alt="repository-stars"/>
-                <img src="https://img.shields.io/github/forks/{{ env('LINK_REPOSITORY') }}?style=flat&color=blue" alt="repository-forks"/>
-                <img src="https://img.shields.io/github/watchers/{{ env('LINK_REPOSITORY') }}?style=flat&color=orange" alt="repository-watchers"/>
-            </div>
-
             <div class="page-header-buttons">
                 <div>
-                    <a class="button is-success" href="javascript:void(0);" onclick="window.vue.scrollTo('a[name=downloads]');">Download</a>
+                    <a class="button is-rounded is-outlined is-large is-success" href="javascript:void(0);" onclick="window.vue.scrollTo('a[name=downloads]');">Download</a>
                 </div>
 
                 <div>
-                    <a class="button is-link" href="javascript:void(0);" onclick="window.vue.scrollTo('a[name=info]');">Read more</a>
+                    <a class="button is-rounded is-outlined is-large is-info" href="javascript:void(0);" onclick="window.vue.scrollTo('a[name=info]');">Read more</a>
                 </div>
+            </div>
+
+            <div class="page-header-badges">
+                <img src="https://img.shields.io/github/stars/{{ env('LINK_REPOSITORY') }}?style=for-the-badge&color=green" alt="repository-stars"/>
+                <img src="https://img.shields.io/github/forks/{{ env('LINK_REPOSITORY') }}?style=for-the-badge&color=blue" alt="repository-forks"/>
+                <img src="https://img.shields.io/github/watchers/{{ env('LINK_REPOSITORY') }}?style=for-the-badge&color=orange" alt="repository-watchers"/>
             </div>
         </div>
     </div>
